d5d3b228943ae4c81821a15c70aee3d38a5d809e
Sushanta K. Pani <panisush@msu.edu>
2020-08-27 17:20:37 -0400

    indic_odi_num_table.h added
    Added numbers to pronunciation entries for Odia.

---
+++ lang/cmu_indic_lang/indic_odi_num_table.h
@@ -0,0 +1,171 @@
+/*************************************************************************/
+/*                                                                       */
+/*                  Language Technologies Institute                      */
+/*                     Carnegie Mellon University                        */
+/*                         Copyright (c) 2015                            */
+/*                        All Rights Reserved.                           */
+/*                                                                       */
+/*  Permission is hereby granted, free of charge, to use and distribute  */
+/*  this software and its documentation without restriction, including   */
+/*  without limitation the rights to use, copy, modify, merge, publish,  */
+/*  distribute, sublicense, and/or sell copies of this work, and to      */
+/*  permit persons to whom this work is furnished to do so, subject to   */
+/*  the following conditions:                                            */
+/*   1. The code must retain the above copyright notice, this list of    */
+/*      conditions and the following disclaimer.                         */
+/*   2. Any modifications must be clearly marked as such.                */
+/*   3. Original authors' names are not deleted.                         */
+/*   4. The authors' names are not used to endorse or promote products   */
+/*      derived from this software without specific prior written        */
+/*      permission.                                                      */
+/*                                                                       */
+/*  CARNEGIE MELLON UNIVERSITY AND THE CONTRIBUTORS TO THIS WORK         */
+/*  DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING      */
+/*  ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT   */
+/*  SHALL CARNEGIE MELLON UNIVERSITY NOR THE CONTRIBUTORS BE LIABLE      */
+/*  FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES    */
+/*  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN   */
+/*  AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,          */
+/*  ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF       */
+/*  THIS SOFTWARE.                                                       */
+/*                                                                       */
+/*************************************************************************/
+/*  Number pronunciation for (Odia) Indic                             */
+/*************************************************************************/
+
+#ifndef _indic_odi_num_table_h_
+#define _indic_odi_num_table_h_
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+#include "indic_num_table.h"
+static const char * const indic_odi_digit[11][2] =
+{
+    { "୦", "ଶୂନ" },
+    { "୧", "ଏକ" },
+    { "୨", "ଦୁଇ" },
+    { "୩", "ତିନି" },
+    { "୪", "ଚାରି" },
+    { "୫", "ପାଂଚ" },
+    { "୬", "ଛଅ" },
+    { "୭", "ସାତ" },
+    { "୮", "ଆଠ" },
+    { "୯", "ନଅ" },
+    { NULL, NULL },
+};
+
+static const char * const indic_odi_two_digit[101][4] =
+{
+    { "୧", "୦", "ଦଶ", NULL },
+    { "୧", "୧", "ଏଗାର", NULL },
+    { "୧", "୨", "ବାର", NULL },
+    { "୧", "୩", "ତେର", NULL },
+    { "୧", "୪", "ଚଉଦ", NULL },
+    { "୧", "୫", "ପନ୍ଦର", NULL },
+    { "୧", "୬", "ଷୋଳ", NULL },
+    { "୧", "୭", "ସତର", NULL },
+    { "୧", "୮", "ଅଠର", NULL },
+    { "୧", "୯", "ଉଣେଇଶ", NULL },
+    { "୨", "୦", "କୋଡ଼ିଏ", NULL },
+    { "୨", "୧", "ଏକୋଇଶ", NULL },
+    { "୨", "୨", "ବାଇଶ", NULL },
+    { "୨", "୩", "ତେଇଶ", NULL },
+    { "୨", "୪", "ଚବିଶ", NULL },
+    { "୨", "୫", "ପଚିଶ", NULL },
+    { "୨", "୬", "ଛବିଶ", NULL },
+    { "୨", "୭", "ସତେଇଶ", NULL },
+    { "୨", "୮", "ଅଠେଇଶ", NULL },
+    { "୨", "୯", "ଅଣତିରିଶ", NULL },
+    { "୩", "୦", "ତିରିଶ", NULL },
+    { "୩", "୧", "ଏକତିରିଶ", NULL },
+    { "୩", "୨", "ବତିଶ", NULL },
+    { "୩", "୩", "ତେତିଶ", NULL },
+    { "୩", "୪", "ଚଉତିରିଶ", NULL },
+    { "୩", "୫", "ପଇଁତିରିଶ", NULL },
+    { "୩", "୬", "ଛତିଶ", NULL },
+    { "୩", "୭", "ସଇଁତିରିଶ", NULL },
+    { "୩", "୮", "ଅଠତିରିଶ", NULL },
+    { "୩", "୯", "ଅଣଚାଳିଶ", NULL },
+    { "୪", "୦", "ଚାଳିଶ", NULL },
+    { "୪", "୧", "ଏକଚାଳିଶ", NULL },
+    { "୪", "୨", "ବୟାଳିଶ", NULL },
+    { "୪", "୩", "ତେୟାଳିଶ", NULL },
+    { "୪", "୪", "ଚଉରାଳିଶ", NULL },
+    { "୪", "୫", "ପଇଁଚାଳିଶ", NULL },
+    { "୪", "୬", "ଛୟାଳିଶ", NULL },
+    { "୪", "୭", "ସତଚାଳିଶ", NULL },
+    { "୪", "୮", "ଅଠଚାଳିଶ", NULL },
+    { "୪", "୯", "ଅଣଞ୍ଚାଷ", NULL },
+    { "୫", "୦", "ପଚାଶ", NULL },
+    { "୫", "୧", "ଏକାବନ", NULL },
+    { "୫", "୨", "ବାବନ", NULL },
+    { "୫", "୩", "ତେପନ", NULL },
+    { "୫", "୪", "ଚଉବନ", NULL },
+    { "୫", "୫", "ପଂଚାବନ", NULL },
+    { "୫", "୬", "ଛପନ", NULL },
+    { "୫", "୭", "ସତାବନ", NULL },
+    { "୫", "୮", "ଅଠାବନ", NULL },
+    { "୫", "୯", "ଅଣଷଠି", NULL },
+    { "୬", "୦", "ଷାଠିଏ", NULL },
+    { "୬", "୧", "ଏକଷଠି", NULL },
+    { "୬", "୨", "ବାଷଠି", NULL },
+    { "୬", "୩", "ତେଷଠି", NULL },
+    { "୬", "୪", "ଚଉଷଠି", NULL },
+    { "୬", "୫", "ପଇଁଷଠି", NULL },
+    { "୬", "୬", "ଛଅଷଠି", NULL },
+    { "୬", "୭", "ଶତଷଠି", NULL },
+    { "୬", "୮", "ଅଠଷଠି", NULL },
+    { "୬", "୯", "ଅନସ୍ତରି", NULL },
+    { "୭", "୦", "ସତୁରି", NULL },
+    { "୭", "୧", "ଏକସ୍ତରି", NULL },
+    { "୭", "୨", "ବାସ୍ତରି", NULL },
+    { "୭", "୩", "ତେସ୍ତରି", NULL },
+    { "୭", "୪", "ଛଉସ୍ତରି", NULL },
+    { "୭", "୫", "ପଞ୍ଚସ୍ତରି", NULL },
+    { "୭", "୬", "ଛଅସ୍ତରି", NULL },
+    { "୭", "୭", "ସତସ୍ତରି", NULL },
+    { "୭", "୮", "ଅଠସ୍ତରି", NULL },
+    { "୭", "୯", "ଅଣାଅଶୀ", NULL },
+    { "୮", "୦", "ଅଶୀ", NULL },
+    { "୮", "୧", "ଏକାଅଶୀ", NULL },
+    { "୮", "୨", "ବୟାଅଶୀ", NULL },
+    { "୮", "୩", "ତେୟାଅଶୀ", NULL },
+    { "୮", "୪", "ଚଉରାଅଶୀ", NULL },
+    { "୮", "୫", "ପଞ୍ଚାଅଶୀ", NULL },
+    { "୮", "୬", "ଛୟାଅଶୀ", NULL },
+    { "୮", "୭", "ସତାଅଶୀ", NULL },
+    { "୮", "୮", "ଅଠାଅଶୀ", NULL },
+    { "୮", "୯", "ଅଣାନବେ", NULL },
+    { "୯", "୦", "ନବେ", NULL },
+    { "୯", "୧", "ଏକାନବେ", NULL },
+    { "୯", "୨", "ବୟାନବେ", NULL },
+    { "୯", "୩", "ତେୟାନବେ", NULL },
+    { "୯", "୪", "ଚଉରାନବେ", NULL },
+    { "୯", "୫", "ପଞ୍ଚାନବେ", NULL },
+    { "୯", "୬", "ଛୟାନବେ", NULL },
+    { "୯", "୭", "ସତାନବେ", NULL },
+    { "୯", "୮", "ଅଠାନବେ", NULL },
+    { "୯", "୯", "ଅନେଶତ", NULL },  
+    { NULL, NULL },
+};
+
+const static indic_num_table odi_num_table = {
+    "odi",
+    &indic_odi_digit,
+    &indic_odi_two_digit,
+    "ଶହ",   /* hundred */
+    "ହଜାର", /* thousand */
+    "ଲକ୍ଷ",  /* lakh */
+    "କୋଟି", /* crore */
+};
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* __cplusplus */
+
+#endif
+
+
